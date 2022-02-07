25e38fc5821d0246ef645066862ce059503ad975
Lemon <74184683+kitty14956590@users.noreply.github.com>
2022-02-07 04:57:07 +0000

    I meow

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def rgb(red,green,blue,text):
 	return '\033[38;2;%s;%s;%sm%s\033[38;2;255;255;255m' % (red,green,blue,text)
 print(rgb(0,154,255,"""                                   __
 https://dolphinonkeys.com      _.-~  )
-                    _..--~~~~,'   ,-/      _
+                    _..--~~~~,'   ,-/     _
                  .-' . . . .'   ,-','    ,' )
                ,' . . . _   ,--~,-'__..-'  ,'
              ,' . . .  (@)' ---~~~~      ,'
@@ -13,19 +13,19 @@ https://dolphinonkeys.com      _.-~  )
          :  . . . .       _     /
         .  . . . .          \`-.:
        .  . . ./  - .          )
-      .   . . |  _____..---.._/ __ Seal :3 __
+      .   . . |  _____..---.._/ __ Seal :3 _
 ~---~~~~----~~~~             ~~\n\n"""))
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-e","--encode", type=str, help="translate into dolphin speak")
-group.add_argument("-d","--decode", type=str, help="translate from dolphin speak")
+group.add_argument("-e","--encode", type=str, help="encode string into dolphin speak")
+group.add_argument("-d","--decode", type=str, help="decode dolphin speak into string")
 args = parser.parse_args()
 try:
 	if args.encode:
-		print(rgb(196,0,255,''.join(bin(ord(i))[2:].zfill(16)for i in args.decode).replace('0','E').replace('1','e')))
+		print(rgb(196,0,255,''.join([bin(ord(i))[2:].zfill(16) for i in [char for char in args.encode]]).translate({48:69,49:101})))
 	elif args.decode:
-		replaced = args.decode.replace("E","0").replace("e","1")
-		print(rgb(196,0,255,''.join(chr(int(replaced[index:index+16],2))for index in range(0,len(replaced),16))))
+		replaced = args.decode.translate({69:48,101:49})
+		print(rgb(196,0,255,''.join([chr(int(replaced[index:index+16],2)) for index in range(0, len(replaced), 16)])))
 	else:
 		print(rgb(255,0,0,"invalid command :c"))
 except Exception as e:
