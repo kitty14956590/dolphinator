3623c01c6103a6ebf20838bdbe8b898fb5770fa3
kitty14956590 <borbtrash@gmail.com>
2024-03-07 08:57:14 +0200

    i forgo

--- test.py
+++ test.py
@@ -9,15 +9,17 @@ if os.getuid() == 0 or os.geteuid() == 0:
 def rgb (a,b,c,text):
 	return "\033[38;2;%s;%s;%sm%s\033[38;2;255;255;255m"%(a,b,c,text)
 
-ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+ansi_remove = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
 for i in glob.glob("./*/main*"):
-	test = ansi_escape.sub('', os.popen(i + " -e \"キティa猫咪喵ニャー\" -n 2>&1 0>&1").read()).replace("\n","")
-	if test == "EEeeEEEEeEeEeeEeEEeeEEEEeeEEEeeEEEeeEEEEeEeEEEeeEEEEEEEEEeeEEEEeEeeeEEeeEEeEeEeeEeEeEeEEeEeEeEeEEeEeEeEeeEeeEeEeEEeeEEEEeeEEeEeeEEeeEEEEeeeEEEeeEEeeEEEEeeeeeeEE":
+	test = os.popen(i + " -e \"キティespañolлтфш1الa猫咪喵ニャーλεμόνι\" -n 2>&1 0>&1").read().lstrip().rstrip()
+	test = ansi_remove.sub("", test)
+	if test == "EEeeEEEEeEeEeeEeEEeeEEEEeeEEEeeEEEeeEEEEeEeEEEeeEEEEEEEEEeeEEeEeEEEEEEEEEeeeEEeeEEEEEEEEEeeeEEEEEEEEEEEEEeeEEEEeEEEEEEEEeeeeEEEeEEEEEEEEEeeEeeeeEEEEEEEEEeeEeeEEEEEEEeEEEEeeeEeeEEEEEeEEEeEEEEeEEEEEEeEEEeEEEeEEEEEEEeEEEeEEeEEEEEEEEEEEEEeeEEEeEEEEEeeEEEeEEeeeEEEEEeeEEeEEEeEEEEEEEEEEEeeEEEEeEeeeEEeeEEeEeEeeEeEeEeEEeEeEeEeEEeEeEeEeeEeeEeEeEEeeEEEEeeEEeEeeEEeeEEEEeeeEEEeeEEeeEEEEeeeeeeEEEEEEEEeeeEeeeEeeEEEEEEeeeEeeEeEeEEEEEEeeeEeeeeEEEEEEEEeeeeEEeeEEEEEEEEeeeEeeeeEeEEEEEEeeeEeeeEEe":
 		print(rgb(0,255,0,"[*] %s passed encode test" % i[2:]))
 	else:
 		print(rgb(255,0,0,"[!] %s failed encode test" % i[2:]))
-	test = ansi_escape.sub('', os.popen(i + " -d \"EEeeEEEEeEeEeeEeEEeeEEEEeeEEEeeEEEeeEEEEeEeEEEeeEEEEEEEEEeeEEEEeEeeeEEeeEEeEeEeeEeEeEeEEeEeEeEeEEeEeEeEeeEeeEeEeEEeeEEEEeeEEeEeeEEeeEEEEeeeEEEeeEEeeEEEEeeeeeeEE\" -n").read()).replace("\n","")
-	if test == "キティa猫咪喵ニャー":
+	test = os.popen(i + " -d \"EEeeEEEEeEeEeeEeEEeeEEEEeeEEEeeEEEeeEEEEeEeEEEeeEEEEEEEEEeeEEeEeEEEEEEEEEeeeEEeeEEEEEEEEEeeeEEEEEEEEEEEEEeeEEEEeEEEEEEEEeeeeEEEeEEEEEEEEEeeEeeeeEEEEEEEEEeeEeeEEEEEEEeEEEEeeeEeeEEEEEeEEEeEEEEeEEEEEEeEEEeEEEeEEEEEEEeEEEeEEeEEEEEEEEEEEEEeeEEEeEEEEEeeEEEeEEeeeEEEEEeeEEeEEEeEEEEEEEEEEEeeEEEEeEeeeEEeeEEeEeEeeEeEeEeEEeEeEeEeEEeEeEeEeeEeeEeEeEEeeEEEEeeEEeEeeEEeeEEEEeeeEEEeeEEeeEEEEeeeeeeEEEEEEEEeeeEeeeEeeEEEEEEeeeEeeEeEeEEEEEEeeeEeeeeEEEEEEEEeeeeEEeeEEEEEEEEeeeEeeeeEeEEEEEEeeeEeeeEEe\" -n").read().lstrip().rstrip()
+	test = ansi_remove.sub("", test)
+	if test == "キティespañolлтфш1الa猫咪喵ニャーλεμόνι":
 		print(rgb(0,255,0,"[*] %s passed decode test" % i[2:]))
 	else:
 		print(rgb(255,0,0,"[!] %s failed decode test" % i[2:]))
